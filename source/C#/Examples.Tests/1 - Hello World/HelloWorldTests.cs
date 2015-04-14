fd25c228f860b4d9b4e0ff10b77a2b111b3a87e4
Eric Burcham <eburcham@eprod.com>
2015-04-14 07:31:38 -0500

    Added Hello Word Example.

---
+++ source/C#/Examples.Tests/1 - Hello World/HelloWorldTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using FluentAssertions;
+
+using NUnit.Framework;
+
+namespace Examples.Tests
+{
+    [TestFixture]
+    public class HelloWorldTests
+    {
+        [Test]
+        public void RunTest()
+        {
+            var helloWorld = new HelloWorld();
+            Action action = () => helloWorld.Run();
+            action.ShouldNotThrow();
+        }
+    }
+}
---
+++ source/C#/Examples/1 - Hello World/HelloWorld.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace Examples
+{
+    public class HelloWorld
+    {
+        public void Run()
+        {
+            var job = new ThreadStart(ThreadJob);
+            var workerThread = new Thread(job);
+            workerThread.Start();
+
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Main thread: {0}", i);
+                Thread.Sleep(200);
+            }
+        }
+
+        private void ThreadJob()
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Worker thread: {0}", i);
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
