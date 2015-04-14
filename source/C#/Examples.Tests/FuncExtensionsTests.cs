1320416483491dfdf24bccfba1647a8a740b025c
Eric Burcham <eburcham@eprod.com>
2015-04-14 08:21:24 -0500

    Added FuncExtensions.Memoize.  Also enabled nuget package restore.

---
+++ source/C#/Examples.Tests/FuncExtensionsTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+using FluentAssertions;
+
+using NUnit.Framework;
+
+namespace Examples.Tests
+{
+    [TestFixture]
+    public class FuncExtensionsTests
+    {
+        [Test]
+        public void MemoizedFunctionShouldOnlyExecuteOnce()
+        {
+            // Arrange
+            var callCount = 0;
+
+            Func<int, int> getSameValue = i =>
+                {
+                    callCount++;
+                    Thread.Sleep(50);
+                    return i;
+                };
+
+            // Act
+            var memoized = getSameValue.Memoize();
+
+            var threads = new List<Thread>();
+            for (var i = 0; i < 1000; i++)
+            {
+                var job = new ThreadStart(
+                    () =>
+                        {
+                            var firstResult = memoized(1);
+                            var secondResult = memoized(2);
+                            firstResult.Should().Be(1);
+                            secondResult.Should().Be(2);
+                        });
+
+                threads.Add(new Thread(job));
+            }
+
+            threads.ForEach(thread => thread.Start());
+            threads.ForEach(thread => thread.Join());
+
+            // Assert
+            callCount.Should().Be(2);
+        }
+    }
+}
---
+++ source/C#/Examples/FuncExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace Examples
+{
+    public static class FuncExtensions
+    {
+        public static Func<TA, TR> Memoize<TA, TR>(this Func<TA, TR> function)
+        {
+            var cache = new ConcurrentDictionary<TA, TR>();
+
+            var lockMap = new ConcurrentDictionary<TA, object>();
+
+            return key =>
+                {
+                    TR result;
+
+                    if (cache.TryGetValue(key, out result))
+                    {
+                        return result;
+                    }
+
+                    var lockObject = lockMap.GetOrAdd(key, new object());
+                    lock (lockObject)
+                    {
+                        result = cache.GetOrAdd(key, function);
+                    }
+
+                    lockMap.TryRemove(key, out lockObject);
+
+                    return result;
+                };
+        }
+    }
+}
