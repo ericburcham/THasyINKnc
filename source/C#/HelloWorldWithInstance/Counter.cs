9083833bc7f152700fcf1975410b18fa5532549f
Eric Burcham <eburcham@eprod.com>
2015-04-14 09:02:10 -0500

    Added Hello World With Instance.

---
+++ source/C#/HelloWorldWithInstance/Counter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace HelloWorldWithInstance
+{
+    public class Counter
+    {
+        public void Count()
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Worker thread: {0}", i);
+                Thread.Sleep(50);
+            }
+        }
+    }
+}
