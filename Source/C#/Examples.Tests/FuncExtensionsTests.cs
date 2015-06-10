7a3ec8e38679bb7f00687806cd7e8964ec1dc57f
Eric Burcham <eburcham@eprod.com>
2015-06-10 10:50:15 -0500

    Made Memoizer test much faster.

--- Source/C#/Examples.Tests/FuncExtensionsTests.cs
+++ Source/C#/Examples.Tests/FuncExtensionsTests.cs
@@ -18,26 +18,26 @@ namespace Examples.Tests
             var callCount = 0;
 
             Func<int, int> getSameValue = i =>
-                {
-                    callCount++;
-                    Thread.Sleep(50);
-                    return i;
-                };
+            {
+                callCount++;
+                Thread.Sleep(1);
+                return i;
+            };
 
             // Act
             var memoized = getSameValue.Memoize();
 
             var threads = new List<Thread>();
-            for (var i = 0; i < 1000; i++)
+            for (var i = 0; i < 50; i++)
             {
                 var job = new ThreadStart(
                     () =>
-                        {
-                            var firstResult = memoized(1);
-                            var secondResult = memoized(2);
-                            firstResult.Should().Be(1);
-                            secondResult.Should().Be(2);
-                        });
+                    {
+                        var firstResult = memoized(1);
+                        var secondResult = memoized(2);
+                        firstResult.Should().Be(1);
+                        secondResult.Should().Be(2);
+                    });
 
                 threads.Add(new Thread(job));
             }
--- Source/C#/Examples/FuncExtensions.cs
+++ Source/C#/Examples/FuncExtensions.cs
@@ -31,19 +31,5 @@ namespace Examples
                     return result;
                 };
         }
-
-        public static Func<TArg1, TArg2, TResult> Memoize<TArg1, TArg2, TResult>(
-            this Func<TArg1, TArg2, TResult> function)
-        {
-            var example = new { TA1 = default(TArg1), TA2 = default(TArg2) };
-            var tuplified = CastByExample(t => function(t.TA1, t.TA2), example);
-            var memoized = tuplified.Memoize();
-            return (a, b) => memoized(new { TA1 = a, TA2 = b });
-        }
-
-        private static Func<TArg, TResult> CastByExample<TArg, TResult>(Func<TArg, TResult> function, TArg example)
-        {
-            return function;
-        }
     }
 }
