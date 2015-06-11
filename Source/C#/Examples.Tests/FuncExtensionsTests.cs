bed4aee41c081bbac445c62f8663461f10f3c0e3
Eric Burcham <eburcham@eprod.com>
2015-06-11 14:10:42 -0500

    Finished adding SynchronizedConcurrentDictionary

--- Source/C#/Examples.Tests/FuncExtensionsTests.cs
+++ Source/C#/Examples.Tests/FuncExtensionsTests.cs
@@ -12,20 +12,20 @@ namespace Examples.Tests
     public class FuncExtensionsTests
     {
         [Test]
-        public void MemoizedFunctionShouldOnlyBeCalledOnce()
+        public void FunctionMemoizedWithMemoizeShouldBeCalledMoreThanOnce()
         {
             // Arrange
             var callCount = 0;
 
-            Func<int, int> getSameValue = i =>
+            Func<int, int> getValueSquared = i =>
             {
                 callCount++;
                 Thread.Sleep(1);
-                return i;
+                return i ^ 2;
             };
 
             // Act
-            var memoized = getSameValue.Memoize();
+            var memoized = getValueSquared.Memoize();
 
             var threads = new List<Thread>();
             for (var i = 0; i < 50; i++)
@@ -36,7 +36,83 @@ namespace Examples.Tests
                         var firstResult = memoized(1);
                         var secondResult = memoized(2);
                         firstResult.Should().Be(1);
-                        secondResult.Should().Be(2);
+                        secondResult.Should().Be(4);
+                    });
+
+                threads.Add(new Thread(job));
+            }
+
+            threads.ForEach(thread => thread.Start());
+            threads.ForEach(thread => thread.Join());
+
+            // Assert
+            callCount.Should().BeGreaterThan(2);
+        }
+
+        [Test]
+        public void FunctionMemoizedWithThreadSafeMemoizeShouldOnlyBeCalledOnce()
+        {
+            // Arrange
+            var callCount = 0;
+
+            Func<int, int> getValueSquared = i =>
+            {
+                callCount++;
+                Thread.Sleep(1);
+                return i ^ 2;
+            };
+
+            // Act
+            var memoized = getValueSquared.ThreadSafeMemoize();
+
+            var threads = new List<Thread>();
+            for (var i = 0; i < 50; i++)
+            {
+                var job = new ThreadStart(
+                    () =>
+                    {
+                        var firstResult = memoized(1);
+                        var secondResult = memoized(2);
+                        firstResult.Should().Be(1);
+                        secondResult.Should().Be(4);
+                    });
+
+                threads.Add(new Thread(job));
+            }
+
+            threads.ForEach(thread => thread.Start());
+            threads.ForEach(thread => thread.Join());
+
+            // Assert
+            callCount.Should().Be(2);
+        }
+
+        [Test]
+        public void FunctionMemoizedWithSyncronizedMemoizeShouldOnlyBeCalledOnce()
+        {
+            // Arrange
+            var callCount = 0;
+
+            Func<int, int> getValueSquared = i =>
+            {
+                callCount++;
+                Thread.Sleep(1);
+                return i ^ 2;
+            };
+
+            // Act
+            var memoized = getValueSquared.SynchronizedMemoize();
+
+            var threads = new List<Thread>();
+            for (var i = 0; i < 50; i++)
+            {
+                var job = new ThreadStart(
+                    () =>
+                    {
+                        var firstResult = memoized(1);
+                        var secondResult = memoized(2);
+                        firstResult.Should().Be(1);
+                        secondResult.Should().Be(4);
                     });
 
                 threads.Add(new Thread(job));
--- Source/C#/Examples/FuncExtensions.cs
+++ Source/C#/Examples/FuncExtensions.cs
@@ -9,7 +9,24 @@ namespace Examples
         {
             var cache = new ConcurrentDictionary<TArg, TResult>();
 
-            var lockMap = new ConcurrentDictionary<TArg, object>();
+            return key =>
+                {
+                    TResult result;
+
+                    if (cache.TryGetValue(key, out result))
+                    {
+                        return result;
+                    }
+
+                    return cache.GetOrAdd(key, function);
+                };
+        }
+
+        public static Func<TArg, TResult> ThreadSafeMemoize<TArg, TResult>(this Func<TArg, TResult> function)
+        {
+            var cache = new ConcurrentDictionary<TArg, TResult>();
+
+            var cacheLock = new ConcurrentDictionary<TArg, object>();
 
             return key =>
                 {
@@ -20,16 +37,23 @@ namespace Examples
                         return result;
                     }
 
-                    var lockObject = lockMap.GetOrAdd(key, new object());
+                    var lockObject = cacheLock.GetOrAdd(key, new object());
                     lock (lockObject)
                     {
                         result = cache.GetOrAdd(key, function);
                     }
 
-                    lockMap.TryRemove(key, out lockObject);
+                    cacheLock.TryRemove(key, out lockObject);
 
                     return result;
                 };
+        }
+
+        public static Func<TArg, TResult> SynchronizedMemoize<TArg, TResult>(this Func<TArg, TResult> function)
+        {
+            var synchronizedCache = new SynchronizedConcurrentDictionary<TArg, TResult>();
+
+            return key => synchronizedCache.GetOrAdd(key, function);
         }
     }
 }
