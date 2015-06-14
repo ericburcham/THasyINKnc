a60a9f4ebdebc2ce6c9ebecbaa2db5ef405f3654
Eric Burcham <jameseburcham@gmail.com>
2015-06-14 15:08:27 -0500

    Fixed failing test.

--- Source/C#/Examples/SynchronizedConcurrentDictionary.cs
+++ Source/C#/Examples/SynchronizedConcurrentDictionary.cs
@@ -6,19 +6,23 @@ namespace Examples
 {
     public class SynchronizedConcurrentDictionary<TKey, TValue> : ConcurrentDictionary<TKey, TValue>
     {
-        private readonly ReaderWriterLockSlim _cacheLock = new ReaderWriterLockSlim();
-
-        public new TValue AddOrUpdate(TKey key, TValue addValue, Func<TKey, TValue, TValue> updateValueFactory)
+        public new TValue GetOrAdd(TKey key, Func<TKey, TValue> valueFactory)
         {
+            TValue result;
+
             _cacheLock.EnterWriteLock();
             try
             {
-                return base.AddOrUpdate(key, addValue, updateValueFactory);
+                result = base.GetOrAdd(key, valueFactory);
             }
             finally
             {
                 _cacheLock.ExitWriteLock();
             }
+
+            return result;
         }
+
+        private readonly ReaderWriterLockSlim _cacheLock = new ReaderWriterLockSlim();
     }
 }
