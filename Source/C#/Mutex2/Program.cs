19679e98a0cbdb90afdd71a49a61dc283c78e36e
Eric Burcham <jameseburcham@gmail.com>
2015-06-16 02:47:37 -0500

    Added mutexes.

---
+++ Source/C#/Mutex2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace Mutex2
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            bool createdNew;
+
+            using (new Mutex(true, @"Local\Mutex", out createdNew))
+            {
+                if (!createdNew)
+                {
+                    Console.WriteLine("Other instance detected.");
+                    Thread.Sleep(5000);
+                    return;
+                }
+
+                Console.WriteLine("We're the only instance running.");
+                for (var i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(i);
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
