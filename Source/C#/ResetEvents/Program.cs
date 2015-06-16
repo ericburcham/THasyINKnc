d3922de3c37d00134f4fc7db7b78389c13ec1103
Eric Burcham <jameseburcham@gmail.com>
2015-06-16 02:28:00 -0500

    Finished WaitHandle example.

---
+++ Source/C#/ResetEvents/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ResetEvents
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var events = new ManualResetEvent[10];
+            for (var i = 0; i < events.Length; i++)
+            {
+                events[i] = new ManualResetEvent(false);
+                var runner = new Runner(events[i], i);
+                var worker = new Thread(runner.Run);
+                worker.Start();
+            }
+
+            var index = WaitHandle.WaitAny(events);
+
+            Console.WriteLine("***** The winner is {0} *****", index);
+
+            WaitHandle.WaitAll(events);
+            Console.WriteLine("All finished!");
+        }
+    }
+
+    internal class Runner
+    {
+        internal Runner(ManualResetEvent resetEvent, int id)
+        {
+            _resetEvent = resetEvent;
+            _id = id;
+        }
+
+        internal void Run()
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                int sleepTime;
+
+                // Don't know, don't care.
+                lock (_lock)
+                {
+                    sleepTime = _random.Next(2000);
+                }
+                Thread.Sleep(sleepTime);
+                Console.WriteLine("Runner {0} at stage {1}", _id, i);
+            }
+
+            _resetEvent.Set();
+        }
+
+        private static readonly object _lock = new object();
+
+        private static readonly Random _random = new Random();
+
+        private readonly ManualResetEvent _resetEvent;
+
+        private readonly int _id;
+    }
+}
