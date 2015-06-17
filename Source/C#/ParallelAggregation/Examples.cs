a7c6b216795fadcb8cc5b0d0f8066c9ffdbda47d
Eric Burcham <jameseburcham@gmail.com>
2015-06-16 19:19:07 -0500

    Finished parallel aggregation examples.

---
+++ Source/C#/ParallelAggregation/Examples.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Threading.Tasks;
+
+namespace ParallelAggregation
+{
+    public class Examples
+    {
+        public void ParallelForSum()
+        {
+            var lockObject = new object();
+            var sumOfSquares = 0;
+
+            Parallel.For(
+                0,
+                10,
+                () => 0,
+                (i, loopState, partialResult) => Square(i) + partialResult,
+                localPartialSum =>
+                    {
+                        lock (lockObject)
+                        {
+                            sumOfSquares += localPartialSum;
+                        }
+                    });
+            Console.WriteLine(sumOfSquares);
+        }
+
+        public void ParallelForeachSum()
+        {
+            var numbers = new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var lockObject = new object();
+            var sumOfSquares = 0;
+
+            Parallel.ForEach(
+                // The enumerable to operate on
+                numbers,
+
+                // The initial partial results
+                () => 0,
+
+                // The iteration definition
+                (i, loopState, partialResult) => Square(i) + partialResult,
+
+                // The final step of each local context
+                localPartialSum =>
+                    {
+                        // Enforce serial access to single, shared result
+                        lock (lockObject)
+                        {
+                            sumOfSquares += localPartialSum;
+                        }
+                    });
+
+            Console.WriteLine(sumOfSquares);
+        }
+
+        public void PlinqSum()
+        {
+            var numbers = new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var sumOfSquares = numbers.AsParallel().Select(i => Square(i)).Sum();
+            Console.WriteLine(sumOfSquares);
+        }
+
+        public void ParallelCustomAggregator()
+        {
+            var numbers = new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var sumOfSquares = numbers.AsParallel().Select(i => Square(i)).Aggregate(0, (sum, value) => sum + value);
+            Console.WriteLine(sumOfSquares);
+        }
+
+        private int Square(int i)
+        {
+            return i * i;
+        }
+    }
+}
---
+++ Source/C#/ParallelAggregation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ParallelAggregation
+{
+    static class Program
+    {
+        static void Main()
+        {
+            var examples = new Examples();
+
+            Console.WriteLine("Parallel sum with custom aggregator");
+            examples.ParallelCustomAggregator();
+
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Parallel sum of for loop");
+            examples.ParallelForSum();
+
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Parallel sum of foreach loop");
+            examples.ParallelForeachSum();
+
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Plinq sum");
+            examples.PlinqSum();
+        }
+    }
+}
