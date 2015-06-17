f652c5fa0ea75680212d8a43ffeb91bac2301283
Eric Burcham <eburcham@eprod.com>
2015-06-17 09:30:07 -0500

    Got a basic AsyncController going.

---
+++ Source/C#/AsyncControllers/Controllers/LocationController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+using AsyncControllers.Models;
+using AsyncControllers.Services;
+
+namespace AsyncControllers.Controllers
+{
+    public class LocationController : AsyncController
+    {
+        public void GetDallasLibrariesAsync()
+        {
+            AsyncManager.OutstandingOperations.Increment();
+            Task.Factory.StartNew(GetLibraries);
+        }
+
+        private void GetLibraries()
+        {
+            var service = new LocationService();
+            var dallasLibraries = service.GetDallasLibrariesInline();
+            this.AsyncManager.Parameters["libraries"] = dallasLibraries;
+            this.AsyncManager.OutstandingOperations.Decrement();
+        }
+
+        public ActionResult GetDallasLibrariesCompleted(IEnumerable<GeoName> libraries)
+        {
+            return View(libraries);
+        }
+    }
+}
--- Source/C#/AsyncControllers/Services/LocationService.cs
+++ Source/C#/AsyncControllers/Services/LocationService.cs
@@ -1,22 +1,22 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
-using System.Web.Mvc;
 
 using AsyncControllers.Models;
 
-namespace AsyncControllers.Controllers.Api
+namespace AsyncControllers.Services
 {
-    public class GeoNamesApiController
+    public class LocationService
     {
-        [HttpGet]
-        public IEnumerable<GeoName> GetLibraries()
+
+        public IEnumerable<GeoName> GetDallasLibrariesInline()
         {
-            return LoadLibraries();
+            return GetDallasLibraries();
         }
 
-        private IEnumerable<GeoName> LoadLibraries()
+        private static IEnumerable<GeoName> GetDallasLibraries()
         {
             var connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
             var dataContext = new GeoDataContext(connectionString);
@@ -29,15 +29,15 @@ namespace AsyncControllers.Controllers.Api
 
             var geoNamesSet = dataContext.Set<GeoName>();
             var q = from m in geoNamesSet
-                    where m.GeoNameId != dallasTx.GeoNameId                                             // We don't want the target in the list of results
-                          && m.FeatureClass == LIBRARY_FEATURE_CLASS                                    // Limit by feature class
-                          && m.FeatureCode == LIBRARY_FEATURE_CODE                                      // Limit by the "Library" feature code
+                    where m.GeoNameId != dallasTx.GeoNameId // We don't want the target in the list of results
+                          && m.FeatureClass == LIBRARY_FEATURE_CLASS // Limit by feature class
+                          && m.FeatureCode == LIBRARY_FEATURE_CODE // Limit by the "Library" feature code
                           && (dallasTx.Latitude >= m.Latitude - LatitudeDeltatToMaxDistance
-                              && dallasTx.Latitude <= m.Latitude + LatitudeDeltatToMaxDistance)         // Limit by latitude
+                              && dallasTx.Latitude <= m.Latitude + LatitudeDeltatToMaxDistance) // Limit by latitude
                           && (dallasTx.Longitude >= m.Longitude - longitudeDeltaToMaxDistance
-                              && dallasTx.Longitude <= m.Longitude + longitudeDeltaToMaxDistance)       // Limit by longitude
-                          && (dallasTx.GeoData.Distance(m.GeoData) / METERS_PER_MILE) <= MAX_DISTANCE   // Limit by EXACT distance
-                    
+                              && dallasTx.Longitude <= m.Longitude + longitudeDeltaToMaxDistance) // Limit by longitude
+                          && (dallasTx.GeoData.Distance(m.GeoData) / METERS_PER_MILE) <= MAX_DISTANCE
+                    // Limit by EXACT distance
                     select m;
 
             return q.OrderBy(match => match.AsciiName).ToList();
