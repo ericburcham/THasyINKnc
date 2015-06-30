ff1113416f322b38e8ef4c35ab69a13b068b4f11
Eric Burcham <eburcham@eprod.com>
2015-06-30 10:30:30 -0500

    Adding basic example.

---
+++ Source/C#/BasicObservableAndObserver/ConsoleObserver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BasicObservableAndObserver
+{
+    public class ConsoleObserver<T> : IObserver<T>
+    {
+        public void OnNext(T value)
+        {
+            Console.WriteLine("Received value: {0}", value);
+        }
+
+        public void OnError(Exception ex)
+        {
+            Console.WriteLine("Received exception: {0}", ex.Message);
+        }
+
+        public void OnCompleted()
+        {
+            Console.WriteLine("Sequence terminated.");
+        }
+    }
+}
---
+++ Source/C#/BasicObservableAndObserver/SequenceOfNumbers.cs
@@ -0,0 +1,7 @@
+﻿namespace BasicObservableAndObserver
+{
+    public class SequenceOfNumbers
+    {
+         
+    }
+}
