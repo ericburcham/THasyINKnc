fcc7f4774c0e90a68c24716ed1d22f5de2165fe2
Eric Burcham <eburcham@eprod.com>
2015-07-09 14:57:22 -0500

    Added some comments...

--- Source/C#/AsyncControllers/Controllers/LocationController.cs
+++ Source/C#/AsyncControllers/Controllers/LocationController.cs
@@ -7,29 +7,40 @@ using AsyncControllers.Services;
 
 namespace AsyncControllers.Controllers
 {
+    /// <summary>
+    /// This is the old-school way, still around for MVC 3 compatibility.
+    /// </summary>
     public class LocationController : AsyncController
     {
+        /// <summary>
+        /// Naming convention is [ActionName]Async
+        /// </summary>
         public void GetDallasLibrariesAsync()
         {
             AsyncManager.OutstandingOperations.Increment();
             Task.Factory.StartNew(GetLibrariesNearDallas);
         }
 
+        /// <summary>
+        /// Naming convention is [ActionName]Completed
+        /// </summary>
+        public ViewResult GetDallasLibrariesCompleted(IEnumerable<GeoName> libraries)
+        {
+            return View(libraries);
+        }
 
         private void GetLibrariesNearDallas()
         {
             var service = new LocationService();
             var dallasLibraries = service.GetDallasLibrariesInline();
-            AsyncManager.Parameters["libraries"] = dallasLibraries;
-            AsyncManager.OutstandingOperations.Decrement();
-        }
-
-        public ViewResult GetDallasLibrariesCompleted(IEnumerable<GeoName> libraries)
-        {
-            return View(libraries);
+            this.AsyncManager.Parameters["libraries"] = dallasLibraries;
+            this.AsyncManager.OutstandingOperations.Decrement();
         }
     }
 
+    ///// <summary>
+    ///// This is the new sauce.
+    ///// </summary>
     //public class LocationController : Controller
     //{
     //    public async Task<ViewResult> GetDallasLibraries()
